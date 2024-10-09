b554c9131abe65119d497fc8c9528c6fde3dd51a
Madhvesh14 <112705770+Madhvesh14@users.noreply.github.com>
2024-10-09 14:17:46 +0530

    Day5

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,15 @@
-import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        LinkedList linkedList = new LinkedList();
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        System.out.println("Enter four elements:");
-        for (int i = 0; i < 4; i++) {
-            linkedList.add(sc.nextInt());
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-        System.out.print("Current List: ");
-        linkedList.display();
-        System.out.print("Enter index to remove: ");
-        int index = sc.nextInt();
-        linkedList.removeAtIndex(index);
-        System.out.print("Updated List: ");
-        linkedList.display();
     }
 }
-class Node {
-    int data;
-    Node next;
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    Node head;
-    void add(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-    void removeAtIndex(int index) {
-        if (head == null || index < 0) return;
-
-        if (index == 0) {
-            head = head.next;
-            return;
-        }
-
-        Node current = head;
-        for (int i = 0; current != null && i < index - 1; i++) {
-            current = current.next;
-        }
-
-        if (current == null || current.next == null) return;
-
-        current.next = current.next.next;
-    }
-
-    void display() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " ");
-            current = current.next;
-        }
-        System.out.println();
-    }
-}
-
